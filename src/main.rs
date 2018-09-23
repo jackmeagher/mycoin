b4a8cd2a183206a0d03be5bb75311577f86c7005
Jack Meagher <jacqueman@gmail.com>
2018-09-23 14:31:51 -0400

    Basic PoWgit add main.rsgit add main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(repeat_generic_slice)]
 extern crate sha1;
 const DIGEST_LENGTH: usize = 20;
+const MAX_BLOCK_SIZE: usize = 1024;
 // First run, we're going to hash a vector of bytes into a 256-bit tuple (4 u64s)
 //type DataBlock = [u8; DIGEST_LENGTH];
 //type DataPad = [u8; DIGEST_LENGTH];
@@ -22,13 +23,34 @@ fn format_bit(bit: bool) -> char {
 }
 
 fn format_byte(byte: &u8) -> String {
-    let mut s = String::new();
+    /*
+    let mut s = String::with_capacity(8);
     for bit in byte_to_bits(byte).iter() {
-        if (*bit) {
+        if *bit {
             s.push('1');
         } else {
             s.push('0');
         }
+    }
+    */
+    format!("{:x?}{:x?}", ((byte >> 4) << 4) / 16u8, byte % 16)
+}
+
+fn format_bytes(bytes: &[u8]) -> String {
+    let mut s = String::with_capacity(bytes.len() * 8);
+    /*
+    for byte in bytes {
+        for bit in byte_to_bits(byte).iter() {
+            if *bit {
+                s.push('1');
+            } else {
+                s.push('0');
+            }
+        }
+    }
+    */
+    for byte in bytes {
+        s.push_str(&format_byte(byte));
     }
     s
 }
@@ -50,11 +72,11 @@ fn byte_to_bits(byte: &u8) -> [bool; 8] {
     ]
 }
 
-fn count_leading_zeros_in_block(bytes: &[u8]) -> u8 {
-    let mut total_zeros: u8 = 0;
+fn count_leading_zeros_in_block(bytes: &[u8]) -> usize {
+    let mut total_zeros: usize = 0;
     for byte in bytes.iter() {
-        let zeros = byte.leading_zeros() as u8;
-        total_zeros += zeros;
+        let zeros = byte.leading_zeros();
+        total_zeros += (zeros as usize);
         if zeros != 8 {
             return total_zeros;
         }
@@ -63,9 +85,12 @@ fn count_leading_zeros_in_block(bytes: &[u8]) -> u8 {
     total_zeros
 }
 
-fn generate_initial_pad(length: usize) -> Option<DataPad> {
-    Some([0u8].repeat(length))
+fn generate_initial_pad_(length: usize) -> DataPad {
+    [0u8].repeat(length)
+}
 
+fn generate_initial_pad(length: usize) -> Option<DataPad> {
+    Some( [0u8].repeat(length) )
 }
 
 fn next_pad(pad: Option<DataPad>) -> Option<DataPad> {
@@ -94,6 +119,25 @@ fn next_pad_(mut pad: DataPad) -> Option<DataPad> {
     return None;
 }
 
+fn next_pad_assume_valid(mut pad: DataPad) -> DataPad {
+    let mut idx: usize = pad.len() - 1;
+    while idx > 0 {
+        if pad[idx] != 255 {
+            pad[idx] += 1;
+            return pad;
+        } else {
+            pad[idx] = 0;
+            idx -= 1;
+        }
+    }
+    // Have to inspect the Most Significant Byte by hand to avoid underflowing usize
+    if pad[0] != 255 {
+        pad[0] += 1;
+        return pad;
+    }
+    panic!()
+}
+
 // Returns (main sum, carry)
 fn add_with_carry(x: u8, y: u8) -> (u8, bool) {
     let sum: u16 = (x as u16) + (y as u16);
@@ -101,43 +145,88 @@ fn add_with_carry(x: u8, y: u8) -> (u8, bool) {
     return (least_significant_byte, sum >= (256u16));
 }
 
-fn xor_blocks(data: &mut DataBlock, pad: &DataPad) {
+// I THINK that xoring and adding the nonces are ultimately the same
+fn xor_blocks(data: &DataBlock, pad: &DataPad) -> DataBlock {
     debug_assert_eq!(data.len(), pad.len());
+
+    let mut xored: Vec<u8> = [ 0u8 ].repeat(data.len());
+
     for i in 0..data.len() {
-        data[i] ^= pad[i];
+        xored[i] = data[i] ^ pad[i];
     }
+
+    xored
+}
+
+fn add_blocks(data: &DataBlock, pad: &DataPad) -> DataBlock {
+    debug_assert_eq!(data.len(), pad.len());
+
+    let length: usize = data.len();
+    let mut sum: Vec<u8> = [ 0u8 ].repeat(length);
+
+    let mut i = length - 1;
+    let mut carry = false;
+    loop {
+        if !carry {
+            let (partial_sum, partial_carry) = add_with_carry(data[i], pad[i]);
+            sum[i] = partial_sum;
+            carry = partial_carry;
+        } else {
+            let temp_sum: u16 = (data[i] as u16) + (pad[i] as u16) + 1u16;
+            sum[i] = temp_sum as u8;
+            carry = temp_sum >= 256u16;
+        }
+        if i == 0 {
+            break;
+        }
+        i -= 1;
+    }
+    sum
+}
+
+fn find_nonce_for_difficulty(data_: &DataBlock, difficulty: usize) -> DataPad {
+    assert!(data_.len() * 8 <= MAX_BLOCK_SIZE);
+    let mut data: DataBlock = Vec::with_capacity(MAX_BLOCK_SIZE / 8);
+    let padding = (MAX_BLOCK_SIZE / 8) - data_.len();
+
+    for _i in 0..padding {
+        data.push( 0u8 );
+    }
+
+    for byte in data_ {
+        data.push( *byte );
+    }
+
+    let mut nonce = generate_initial_pad_(MAX_BLOCK_SIZE / 8);
+
+    assert!(nonce.len() == data.len());
+
+    loop {
+        let sum = add_blocks(&data, &nonce);
+        let digest = hash(&sum[..]);
+        let zeros = count_leading_zeros_in_block(&digest);
+        if zeros >= difficulty {
+            println!("Found nonce! difficulty={}", difficulty);
+            println!("Data  : {}", format_bytes(&data[..]));
+            println!("Nonce : {}", format_bytes(&nonce[..]));
+            println!("Sum   : {}", format_bytes(&sum[..]));
+            println!("Digest: {}\n", format_bytes(&digest));
+            break
+        }
+        nonce = next_pad_assume_valid(nonce);
+    }
+
+    nonce
 }
 
 
 fn main() {
-    let msg = "Hello, world!";
-    println!("{}", msg);
-    let h = hash(b"Hello, world!");
-    println!("{}", format_bit(true));
-    
-    for byte in h.iter() {
-        print_byte(byte);
-    }
-    println!("{}", 32 as u8);
-    print_byte(&(32 as u8));
-    print_byte(&(64 as u8));
-    print_byte(&(65 as u8));
-    print_byte(&(0b10101010 as u8));
+    let nonce = find_nonce_for_difficulty(&[123u8].repeat(MAX_BLOCK_SIZE/8), 23);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    mod invariants {
-        use super::DIGEST_LENGTH;
-
-        #[test]
-        fn digest_is_less_than_256_bits() {
-            // This is to make it okay to use u8 as the return type of count_leading_zeros
-            assert!(DIGEST_LENGTH*8 < 256);
-        }
-    }
 
     mod format_byte {
         use super::format_byte;
@@ -202,10 +291,29 @@ mod tests {
         use super::generate_initial_pad;
         use super::next_pad;
         use super::DIGEST_LENGTH;
+        use super::count_leading_zeros_in_block;
+
+        #[test]
+        fn counts_zeros_correctly() {
+            assert_eq!(0, count_leading_zeros_in_block(&[ 255u8 ]));
+            assert_eq!(1, count_leading_zeros_in_block(&[ 0b01111111u8 ]));
+            assert_eq!(2, count_leading_zeros_in_block(&[ 0b00111111u8 ]));
+            
+            assert_eq!(8, count_leading_zeros_in_block(&[ 0u8, 255u8 ]));
+            assert_eq!(9, count_leading_zeros_in_block(&[ 0u8, 0b01111111u8 ]));
+            assert_eq!(10, count_leading_zeros_in_block(&[ 0u8, 0b00111111u8 ]));
+
+            assert_eq!(160, count_leading_zeros_in_block(&[
+                                                         0u8, 0u8, 0u8, 0u8,
+                                                         0u8, 0u8, 0u8, 0u8,
+                                                         0u8, 0u8, 0u8, 0u8,
+                                                         0u8, 0u8, 0u8, 0u8,
+                                                         0u8, 0u8, 0u8, 0u8 ]));
+        }
 
         #[test]
         fn gives_correct_first_pad() {
-            let mut v: Vec<u8> = Vec::new();
+            let v: Vec<u8> = Vec::new();
             assert_eq!(
                 generate_initial_pad(0).unwrap(),
                 v);
@@ -283,6 +391,72 @@ mod tests {
                   255u8, 255u8, 255u8, 255u8,
                   255u8, 255u8, 255u8, 255u8 ]));
             assert_eq!(test_val, None);
+        }
+
+        use super::xor_blocks;
+        use super::add_blocks;
+
+        #[test]
+        fn xor_simple_blocks() {
+            let result1 = xor_blocks(
+                &vec![ 0u8, 255u8 ],
+                &vec![ 255u8, 0u8 ]
+            );
+            assert_eq!(result1, vec![ 255u8, 255u8 ]);
+
+            let result2 = xor_blocks(
+                &vec![ 255u8, 255u8 ],
+                &vec![ 255u8, 255u8 ]
+            );
+            assert_eq!(result2, vec![ 0u8, 0u8 ]);
+
+            let result3 = xor_blocks(
+                &vec![ 0b10101010u8, 0b10101010u8 ],
+                &vec![ 0b01010101u8, 0b11110000u8 ]
+            );
+            assert_eq!(result3, vec![ 255u8, 0b01011010u8 ]);
+        }
+
+        #[test]
+        fn add_simple_blocks() {
+            let result1 = add_blocks(
+                &vec![ 0u8, 128u8 ],
+                &vec![ 0u8, 128u8 ]
+            );
+            assert_eq!(result1, vec![ 1u8, 0u8 ]);
+
+            let result2 = add_blocks(
+                &vec![ 1u8, 128u8 ],
+                &vec![ 0u8, 128u8 ]
+            );
+            assert_eq!(result2, vec![ 2u8, 0u8 ]);
+
+            let result3 = add_blocks(
+                &vec![ 0u8, 128u8 ],
+                &vec![ 1u8, 128u8 ]
+            );
+            assert_eq!(result3, vec![ 2u8, 0u8 ]);
+
+            let result4 = add_blocks(
+                &vec![ 0u8, 127u8 ],
+                &vec![ 0u8, 128u8 ]
+            );
+            assert_eq!(result4, vec![ 0u8, 255u8 ]);
+
+            let result5 = add_blocks(
+                &vec![ 0u8, 129u8 ],
+                &vec![ 0u8, 128u8 ]
+            );
+            assert_eq!(result5, vec![ 1u8, 1u8 ]);
+        }
+
+        #[test]
+        fn add_blocks_discards_total_carry(){
+            let result1 = add_blocks(
+                &vec![ 255u8, 255u8 ],
+                &vec![ 1u8  , 0u8   ]
+            );
+            assert_eq!(result1, vec![ 0u8, 255u8 ])
         }
     }
 
